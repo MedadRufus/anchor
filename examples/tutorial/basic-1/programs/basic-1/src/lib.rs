0cd09c800db122bec746d99a9232f6651a0645e7
MedadRufus <medadrufus@gmail.com>
2021-11-26 12:12:26 +0000

    use new key

--- examples/tutorial/basic-1/programs/basic-1/src/lib.rs
+++ examples/tutorial/basic-1/programs/basic-1/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("44gPq2a4Ss7rBcg6WTsDJCzA1baTFaytVpxNSDGrsy3t");
 
 #[program]
 mod basic_1 {
